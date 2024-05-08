5948871504a8b0dcc1b2b2762d930a71bcb1e440
Korosensee <francisco.girard@uap.edu.ar>
2024-05-07 21:48:57 -0300

    Interface XD

--- src/app/recursos/page.tsx
+++ src/app/recursos/page.tsx
@@ -53,12 +53,12 @@ export default function Edificios() {
     <main className="container mx-auto flex flex-col pt-10 bg-white">
 		<h1 className="text-5xl text-blue-600 font-extrabold text-center">Recursos</h1>
 		<form action="/api/pokemon" method="post" onSubmit={addRecursO}>
-			<h2 className="text-2xl text-blue-700 font-bold">Agregar nuevo Edificio</h2>
+			<h2 className="text-2xl text-blue-700 font-bold">Agregar nuevo Recurso</h2>
 			<input type="text" name="name" placeholder="Name" className="my-1 w-full p-2 border border-gray-300 rounded-lg" />
 			<button type="submit" className="w-full p-2 bg-green-500 text-white rounded-lg mt-2 font-bold uppercase duration-200 hover:bg-blue-700">Agregar</button>
 		</form>
 		<ul className="mt-4 border-4 border-black-700">
-            <h1>Edificios</h1>
+            <h1>Recursos</h1>
 			<li className="flex items-center justify-between border-b border-gray-300 p-2 bg-green-700">
 				<span className="text-lg text-white font-extrabold w-1/3">ID</span>
 				<span className="text-lg text-white font-extrabold w-1/3 text-center">Name</span>
