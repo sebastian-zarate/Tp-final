c41578110f4b8c61aa8ff6ca011547e899a4f713
AlanGoltz <alan.benner@uap.edu.ar>
2024-05-20 09:26:06 -0300

    epicante-koro-colaboration

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -8,7 +8,7 @@ async function Signup(){
         "use server"
         const email =data.get('email') as string
         const password =data.get('password') as string
-        const username = data.get('username') as string
+        const username =data.get('username') as string
         const user = {
           email: email,
           password: password,
@@ -25,7 +25,11 @@ async function Signup(){
         <div className=" flex flex-col mt-16 p-8 items-center justify-center bg-gray-400">
           
           <h1 className=" border-solid text-xl  text-white bg-gray-400">Signup</h1>
-          <form  action={createUs}>  
+          <form  action={createUs}> 
+          <label id= 'userLabel' htmlFor="username">Username</label>
+            <p/>
+            <input className="border px-2" type="text" id="username" name="username" required></input>
+            <p/> 
             <label id='emailLabel' htmlFor="email">Email</label>
             <p/>
             <input className="border px-2" type="email" id="email" name="email" required></input>
@@ -34,17 +38,13 @@ async function Signup(){
             <p/>
             <input className="border px-2" type="password" id="password" name="password" required></input>
             <p/>
-            <label id= 'userLabel' htmlFor="username">Username</label>
-            <p/>
-            <input className="border px-2" type="text" id="username" name="username" required></input>
-            <p/>
-
+            
             <button className=" ml-12 mt-5 bg-blue-600 text-white px-2 py-1" type="submit">Registrarse</button>
           </form>   
           <a href="/login" className=" m-5 text-sm text-white">Ya tengo mi cuenta creada</a>   
           <div className=" p-3 text-sm border flex flex-col">
             <span  id="em_caract">El email es mayor a 5 caracteres?</span>
-            <span id="em_simb">El email incluye el simbolo "@"?</span>
+            <span id="em_simb">Es email incluye el simbolo "@"?</span>
             <span id="cont_caract">La contraseña es mayor a 8 caracteres?</span>
             <span id = "use_caract">El username es mayor a 3 caracteres?</span>
           </div>     
--- src/services/users.ts
+++ src/services/users.ts
@@ -2,7 +2,6 @@
 import { PrismaClient } from "@prisma/client"
 
 import { getSalt, hashPassword } from "../helpers/hashPassword";
-import { ok } from "assert";
 import { cookies } from "next/headers";
 import { signJWT } from "@/helpers/jwt";
 
@@ -10,22 +9,21 @@ const prisma = new PrismaClient()
 let cantMadera = 500
 let cantPan = 800
 let cantPiedra= 600
+let unidadesDeTrabajo = 100
 
 export async function createUser(user: { email: string, password: string, username: string}) {
-  if (!user.email || user.email.length < 5 || !user.email.includes('@')) {
+  if (!user.email || user.email.length < 5 || !user.email.includes('@') ) {
     throw new Error('Invalid email');
-    
   }
   if (!user.username || user.username.length < 3){
     throw new Error('Invalid username');
   }
-
-  const existingEmail = await prisma.users.findFirst({
+  const existing = await prisma.users.findFirst({
     where:{
       email:user.email
     }
   })
-  if (existingEmail) {
+  if (existing) {
     throw new Error('User already exists');
   }
   const existingUsername = await prisma.users.findFirst({
@@ -51,39 +49,92 @@ export async function createUser(user: { email: string, password: string, userna
     pan: cantPan,
     madera: cantMadera,
     nivel:1, 
-    salt
+    salt,
+    unidadesDeTrabajo
 }
 
     await prisma.users.create({data: userWithHash});    
 }
 
-export async function authenticateUser(user: { email: string, username?: string, password: string }) {
-  let existing;
-//Buscar por email o por username 
-  if(user.email){
-  existing = await prisma.users.findFirst({
+export async function authenticateUser(user: {username:string, email: string, password: string }) {
+
+  const existing = await prisma.users.findFirst({
     where:{
       email:user.email
     }
   })
-}else if (user.username){
-  existing = await prisma.users.findFirst({
-    where: {
-      username: user.username
-}
-  })
-}else {
-  throw new Error('Email or username required')
-}
-if (!existing) {
-  throw new Error('User not found');
-}
+  if (!existing) {
+    throw new Error('User not found');
+  }
   const hash = hashPassword(existing.salt + user.password);
   console.log("el hash nuevo: ",hash)
   console.log("el hash existente: ",existing.hash)
   if (hash !== existing.hash) {
     throw new Error('Invalid password');
   }
-  cookies().set('user', signJWT(hash) , { httpOnly: true, sameSite: 'strict' })
-  return { email: existing.email, username: existing.username};
+  
+  cookies().set("user" , signJWT(hash) , { httpOnly: true, sameSite: 'strict' })
+  cookies().set("userName" , user.username , { httpOnly: true, sameSite: 'strict' })
+  /* cookies().set("userName" , user.email , { httpOnly: true, sameSite: 'strict' }) */
+/*   if(user.email){
+    cookies().set(user.email, signJWT(hash) , { httpOnly: true, sameSite: 'strict' })
+  } 
+  if(user.username){
+    cookies().set(user.username, signJWT(hash) , { httpOnly: true, sameSite: 'strict' })
+  }
+ */
+ 
+
+  return { email: existing.email, username: existing.username };
+}
+
+
+export const getUserByUserName = async (userName:string) => {
+  const users = await prisma.users.findFirst({
+    where: {
+      username: userName
+    }
+  })        
+  return users
+}
+export const getUserByemail = async (email:string) => {
+  const users = await prisma.users.findFirst({
+    where: {
+      email: email
+    }
+  })   
+   return users
+}
+//método que me devuelve un booleano de si existe el usuario
+export const getBoolUserExist = async (email:string) => {
+  const users = await prisma.users.findFirst({
+    where: {
+      email: email
+    }
+  })  
+  if(users)  return true
+  else return false    
+}
+
+export const updateUnidAtaqUser =async (id:string, data: any) => {
+  const users = await prisma.users.update({
+    where: {
+      id: id
+    },
+    data: {
+      unidadesDeAtaque: data
+    }
+  })
+  return users
+}
+export const updateUnidDefUser =async (id:string, data: any) => {
+  const users = await prisma.users.update({
+    where: {
+      id: id
+    },
+    data: {
+      unidadesDeDefensa: data
+    }
+  })
+  return users
 }
