b610a7b7074cd7f4c4342674721c2618955cd247
Mateo <maldonado12net@gmail.com>
2024-06-05 02:07:00 -0300

    recursos automaticos funcionando

--- src/app/principal/dynamicButton.tsx
+++ src/app/principal/dynamicButton.tsx
@@ -4,8 +4,8 @@ import MenuDesplegable from './menuDesplegable';
 import MenuAsignar from './menuAsignar';
 import Mensajeria from './menuChats';
 import { GuardarEdificio, getBuildingsByUserId, builtEdificio, getUEbyUserId, getUEById } from '../../services/userEdificios';
-import { getUserByCooki, getUser, getUserByHash, updateUserBuildings} from '@/services/users';
-import {recolectarRecursos } from '@/services/recursos';
+import { getUserByCooki, getUser, getUserByHash, updateUserBuildings, updateUser} from '@/services/users';
+import {recolectarRecursos, calcularMadera, calcularPiedra, calcularPan } from '@/services/recursos';
 import { getChats, getUsernameOther, getChatName } from '@/services/chats';
 import { getMensajes } from '@/services/mensajes';
 
@@ -40,8 +40,17 @@ const DynamicBuildings: React.FC = () => {
   const[menuButton, setMenBut] = useState(false);
   const[menuButton2, setMenBut2] = useState("");
   
-  
-   ////-----------------------------------------------------------
+// VARIABLES PARA LA RECOLECCION DE RECURSOS AUTOMATICA
+const [maderaPorSegundo, setMaderaPorSegundo] = useState(0);
+const [piedraPorSegundo, setPiedraPorSegundo] = useState(0);
+const [panPorSegundo, setPanPorSegundo] = useState(0);
+const maderaRef = useRef(madera);
+const piedraRef = useRef(piedra);
+const panRef = useRef(pan);
+
+
+
+////-----------------------------------------------------------
 //---------------------seba--------------------------------------
 //-----------------------------------------------------------
 const [canon, setCanon] = useState(0);
@@ -104,6 +113,64 @@ const [message, setMessage] = useState('');
     }
     fetchData();
   }, [userId]);
+
+
+  //useffects recursos automaticos
+ //useffect para recolectar recursos automaticamente CAMBIAR 50 POR 5
+ useEffect(() => {
+  const fetchResource = async (calculateFunc: (id: string) => Promise<number>, setFunc: (value: number) => void) => {
+    try {
+      const result = await calculateFunc(userId);
+      setFunc(result);
+    } catch (error) {
+      console.error(`Error fetching resource: ${error}`);
+    }
+  };
+
+  if (userId) {
+    Promise.all([
+      fetchResource(calcularMadera, setMaderaPorSegundo),
+      fetchResource(calcularPiedra, setPiedraPorSegundo),
+      fetchResource(calcularPan, setPanPorSegundo),
+    ]);
+  }
+}, [userId]);
+
+useEffect(() => {
+  cargarUser();
+  const timer = setInterval(() => {
+    setMadera(madera => madera + maderaPorSegundo);
+    setPiedra(piedra => piedra + piedraPorSegundo);
+    setPan(pan => pan + panPorSegundo);
+  }, 2000);
+
+  return () => clearInterval(timer);
+}, [maderaPorSegundo, piedraPorSegundo, panPorSegundo]);
+
+useEffect(() => {
+  maderaRef.current = madera;
+}, [madera]);
+
+useEffect(() => {
+  piedraRef.current = piedra;
+}, [piedra]);
+
+useEffect(() => {
+  panRef.current = pan;
+}, [pan]);
+
+useEffect(() => {
+  const timer = setInterval(async () => {
+    try {
+      await updateUser(userId, { madera: maderaRef.current, piedra: piedraRef.current, pan: panRef.current });
+      console.log('recursos actualizados');
+    } catch (error) {
+      console.error(`Error updating user: ${error}`);
+    }
+  }, 5000);
+
+  return () => clearInterval(timer);
+}, [userId]);
 
 //conseguir todos los nombres de los chats
 useEffect(() => {
@@ -423,10 +490,10 @@ const handleBuildClick = async (id: string, x: number, y: number, buildingType: 
   return (
     <div className="hola flex flex-col items-center justify-center w-screen h-screen bg-gray-900">
       <div className="absolute top-0 left-0 p-4 bg-red-500 text-blue font-bold py-2 px-4 rounded">
-        <h3>Usuario: {usuario}</h3>
-        <h3>Madera: {madera}</h3>
-        <h3>Piedra: {piedra}</h3>
-        <h3>Pan: {pan}</h3>
+      <h3>Usuario: {usuario}</h3>
+        <h3>Madera: {madera} || PS: {maderaPorSegundo}  </h3>
+        <h3>Piedra: {piedra} || PS: {piedraPorSegundo}  </h3>
+        <h3>Pan:    {pan}    || PS: {panPorSegundo}     </h3>
         <h3>Trabajadores disponibles: {unidadesDisponibles}</h3>
         <button onClick={() => recolectarRecursosUser()}> Recolectar Recursos</button>        
       </div>
